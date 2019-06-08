b2a24b7e67e311a3ad51da0115efd314fd6d9d7e
FuYuCh <51389261+FuYuCh@users.noreply.github.com>
2019-06-08 10:25:16 +0800

    使用UI來測試 HP增加跟減少

---
+++ UnityUITetx/Assets/HP.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  
+
+public class HP : MonoBehaviour
+{
+    public Text HPText;  //宣告Text物件
+
+    public float hp;
+    //程式一開就會執行的事件
+    void Start()
+    {
+        DisplayHP();
+    }
+    void Update()
+    {
+        
+    }
+    //-HP的方法
+    public void Sleep()  
+    {
+        hp += 10;
+        DisplayHP();
+    }
+
+    //-HP的方法
+    public void BeAttacked()
+    {
+        hp -= 10.6f;
+        DisplayHP();
+    }
+    //顯示目前HP
+    public void DisplayHP()
+    {
+        HPText.text = hp.ToString("#");
+    }
+    
+}
